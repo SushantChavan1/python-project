10814d5d15bc540c1f9ddae60e583df7cdf2b17a
Sushant Chavan <sushantchavan120@gmail.com>
2023-03-19 21:36:12 +0530

    large number between 3

---
+++ large_3.py
@@ -0,0 +1,12 @@
+def large_3():
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    c=(int(input("Enter the value of c")))
+    if(a>b and a>c):
+        print("The a is large")
+    elif(b>c):
+        print("The b is large")
+    else:
+        print("The c is large")
+
+large_3()
