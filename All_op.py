c764dde29b359c3967961e3f3889365dcd278970
Sushant Chavan <sushantchavan120@gmail.com>
2023-03-19 21:03:27 +0530

    simple code

---
+++ All_op.py
@@ -0,0 +1,29 @@
+#All arethmatics by an user defined function
+def add():#function defination
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    print("The Addition=",a+b)
+
+def sub():#function defination
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    print("The Addition=",a-b)
+
+def multi():#function defination
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    print("The Addition=",a*b)
+
+def div():#function defination
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    print("The Addition=",a/b)
+
+print("Addition of two number")
+add()#function call
+print("Substraction of two number")
+sub()#function call
+print("Multiplication of two number")
+multi()#function call
+print("Division of two numbers")
+div()#function call
