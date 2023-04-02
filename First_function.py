8aa60cb0861c7c219664caf29de03c15d25ab546
Sushant Chavan <sushantchavan120@gmail.com>
2023-04-02 21:29:49 +0530

    function 1 type

---
+++ First_function.py
@@ -0,0 +1,8 @@
+#1.function without return without argument in python 
+def addition():#defination of function
+    a=(int(input("Enter the value of a")))
+    b=(int(input("Enter the value of b")))
+    c=a+b
+    print("The addition=",c)
+
+addition()#call of function
