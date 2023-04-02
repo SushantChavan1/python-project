c8f49f862f7a69dc917c10fe9cce7256022677ed
Sushant Chavan <sushantchavan120@gmail.com>
2023-04-02 21:30:57 +0530

    function 2 type

---
+++ Second_function.py
@@ -0,0 +1,7 @@
+"""2.function with Argument Without Return"""
+def addition(a,b):#function def with argument
+    print("The addition=",a+b)
+
+p=(int(input("Enter the value of p")))
+q=(int(input("Enter the value of q")))
+addition(p,q)#here we can pass actula parameters to function call
